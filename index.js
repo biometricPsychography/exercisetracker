4a863284226431249b19435c53639ac5a800fc4f
biometricPsychography <cyclinglinguist@gmail.com>
2022-10-19 13:00:50 -0500

    Create view for database issues

--- index.js
+++ index.js
@@ -1,18 +1,80 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const port = 8080;
+const mongoose = require('mongoose');
 require('dotenv').config()
+
 
 app.use(cors())
 app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
-});
+
+
+
+main().catch(err => console.log(err));
+
+async function main() {
+  app.get('/', (req, res) => {
+
+
+    if (!dbConnectionStatus) {
+      res.sendFile(__dirname + '/views/preindex.html')
+    } else {
+      res.sendFile(__dirname + '/views/index.html')
+    }
+  });
+
+
+  let dbConnectionStatus = await mongoose.connect(`mongodb+srv://${process.env.UN}:${process.env.PW}@cluster0.aaqngpq.mongodb.net/exercisetracker`);
+  
+
+
+
+
+  const userSchema = new mongoose.Schema({
+    name: String,
+    exercises: [{
+      description: String,
+      duration: {
+        type     : Number,
+        required : true,
+        unique   : false,
+        validate : {
+          // Pretty sure the Number below is not the same as the Number of type:
+          validator : Number.isInteger,
+          message   : '{VALUE} is not an integer value'
+        }
+      }
+    }] 
+  });
+
+  const User = mongoose.model('User', userSchema);
+
+  const silence = new User({name: "Silence"});
+
+  await silence.save();
+  
 
 
 
 
 
-const listener = app.listen(process.env.PORT || 3000, () => {
+  app.post('/api/shorturl', function (req, res) {
+
+  });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+const listener = app.listen(port, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
